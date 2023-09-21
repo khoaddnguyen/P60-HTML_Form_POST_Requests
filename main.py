60a7a70d767c2c365fa42900da4c4c7033fdbe85
kwa_laa <khoaddnguyen@gmail.com>
2023-09-21 13:03:00 -0500

    successfully pass the html form to the login page via jinja

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route('/login', methods=["POST"])
+@app.route("/login", methods=["POST"])
 def receive_data():
-    name = request.form['fullname'],
-    password = request.form['password']
+    name = request.form["fullname"]
+    password = request.form["password"]
     return f"<h1>Name: {name}, Password: {password}</h1>"
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
